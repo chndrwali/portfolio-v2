6c5316f8891e04f41f96acefbba3ec7dec8d5fa0
chndrwali <chndrwali@gmail.com>
2024-03-20 14:31:21 +0700

    add: hero image

--- components/Hero/DevImg.jsx
+++ components/Hero/DevImg.jsx
@@ -1,5 +1,11 @@
-const DevImg = () => {
-    return ( <>devImg</> );
+import Image from "next/image";
+
+const DevImg = ({containerStyles, imgSrc}) => {
+    return ( 
+        <div className={`${containerStyles}`}>
+            <Image src={imgSrc} fill priority alt="Candra" /> 
+        </div>
+     );
 }
  
 export default DevImg;
