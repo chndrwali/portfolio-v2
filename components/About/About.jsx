64ebc0e4bcb4a781a1e77a22796740345a094afc
chndrwali <chndrwali@gmail.com>
2024-03-20 17:02:59 +0700

    add: table

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,5 +1,4 @@
 import DevImg from '../Hero/DevImg';
-import Image from 'next/image';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { User2, MailIcon, HomeIcon, PhoneCall, GraduationCap, Calendar, Briefcase } from 'lucide-react';
 
@@ -22,15 +21,11 @@ const infoData = [
   },
   {
     icon: <GraduationCap size={20} />,
-    text: 'Informatics Engineering',
+    text: 'S1 Informatics Engineering',
   },
   {
     icon: <HomeIcon size={20} />,
     text: 'Jl. Mentor N0.74 Bandung, Indonesia',
-  },
-  {
-    icon: <User2 size={20} />,
-    text: 'Candra Wali Sanjaya',
   },
 ];
 
@@ -129,9 +124,47 @@ const About = () => {
 
   console.log(getData(qualificationData, 'education'));
   return (
-    <section className='xl:h-[860px] pb-12 xl:py-24'>
-      <div className='container mx-auto'>
-        <h2 className='section-title mb-8 xl:mb-16 text-center mx-auto'>About me</h2>
+    <section className="xl:h-[860px] pb-12 xl:py-24">
+      <div className="container mx-auto">
+        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">About me</h2>
+        <div className="flex flex-col xl:flex-row ">
+          <div className="hidden xl:flex flex-1 relative">
+            <DevImg containerStyles="bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-no-repeat relative" imgSrc="/hero/developer.webp" />
+          </div>
+          <div className="flex-1">
+            <Tabs defaultValue="personal">
+              <TabsList className="w-full grid xl:grid-cols-3 xl:max-w-[520px] xl:border dark:border-none ">
+                <TabsTrigger className="w-[162px] xl:w-auto" value="personal">
+                  Personal Info
+                </TabsTrigger>
+                <TabsTrigger className="w-[162px] xl:w-auto" value="qualifications">
+                  Qualifications
+                </TabsTrigger>
+                <TabsTrigger className="w-[162px] xl:w-auto" value="skills">
+                  Skill
+                </TabsTrigger>
+              </TabsList>
+              <div className="text-lg mt-12 xl:mt-8 ">
+                <TabsContent value="personal">
+                  <div className='text-center xl:text-left'>
+                    <h3 className="h3 mb-4">Unmatched Service Quality for Over 10 Years</h3>
+                    <p className="subtitle max-w-xl mx-auto xl:mx-0 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, qui!</p>
+                    <div className='grid xl:grid-cols-2 gap-4 mb-12'>
+                        {infoData.map((item, index) =>{
+                            return <div key={index} className='flex items-center gap-x-4 mx-auto xl:mx-0'>
+                                <div className='text-primary'>{item.icon}</div>
+                                <div>{item.text}</div>
+                            </div>
+                        })}
+                    </div>
+                  </div>
+                </TabsContent>
+                <TabsContent value="qualifications">Qualifications</TabsContent>
+                <TabsContent value="skills">Skill</TabsContent>
+              </div>
+            </Tabs>
+          </div>
+        </div>
       </div>
     </section>
   );
