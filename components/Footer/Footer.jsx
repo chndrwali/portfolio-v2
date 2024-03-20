139ec142818f52576d584e5fb71727b031d92793
chndrwali <chndrwali@gmail.com>
2024-03-20 20:29:13 +0700

    add: moment and footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,5 +1,17 @@
+import moment from 'moment';
+import Socials from '../Hero/Socials';
+
 const Footer = () => {
-    return ( <footer>Footer</footer> );
-}
- 
+  return (
+    <footer className="bg-secondary py-12">
+      <div className="container mx-auto ">
+        <div className='flex flex-col items-center justify-between'>
+          <Socials containerStyles='flex gap-x-6 mx-auto xl:mx-0 mb-4' iconsStyles='text-primary dark:text-white/70 text-[20px] hover:text-white dark:hover:text-primary transition-all' />
+          <div className='text-muted-foreground text-white'>Copyright &copy; {moment().format('YYYY')} Candra Wali Sanjaya | All right reserved.</div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
 export default Footer;
