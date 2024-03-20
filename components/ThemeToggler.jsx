cbb4286d3671b58f491d9d07d5065e2c0bb84f92
chndrwali <chndrwali@gmail.com>
2024-03-20 11:34:45 +0700

    add: scroll progress

--- components/ThemeToggler.jsx
+++ components/ThemeToggler.jsx
@@ -1,19 +1,20 @@
-'use client'
+'use client';
 
-import { Button } from "./ui/button";
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
-import { useTheme } from "next-themes";
+import { Button } from './ui/button';
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
+import { useTheme } from 'next-themes';
 
 const ThemeToggler = () => {
-    const {theme, setTheme} = useTheme();
+  const { theme, setTheme } = useTheme();
 
+  return (
+    <div>
+      <Button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+        <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
+        <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 " />
+      </Button>
+    </div>
+  );
+};
 
-    return ( <div>
-        <Button>
-            <SunIcon />
-            <MoonIcon />
-        </Button>
-    </div> );
-}
- 
 export default ThemeToggler;
