ef652246fe4c87d66a33eaa1023088b96193518d
chndrwali <chndrwali@gmail.com>
2024-03-20 12:51:02 +0700

    add: logo

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,5 +1,12 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 const Logo = () => {
-    return ( <>Logo</> );
-}
- 
+  return (
+    <Link href="/">
+      <Image src="/logo.webp" width={54} height={54} priority alt="Logo" className="rounded-tr-2xl rounded-bl-2xl" />
+    </Link>
+  );
+};
+
 export default Logo;
