5adefab19c54f8dcb821832444d9c2c64d2696db
chndrwali <chndrwali@gmail.com>
2024-03-20 19:25:17 +0700

    add: slider

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import About from "@/components/About/About";
 import Hero from "@/components/Hero/Hero";
 import Services from "@/components/Services/Services";
+import Work from "@/components/Work/Work";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
+      <Work />
     </main>
   );
 }
--- components/Services/Services.jsx
+++ components/Services/Services.jsx
@@ -28,7 +28,7 @@ const Services = () => {
             return (
               <Card key={index} className="w-full max-w-[420px] h-[300px] flex flex-col pt-16 pb-10 justify-center items-center relative">
                 <CardHeader className="text-primary absolute -top-[60px] "> 
-                <div className="w-[140px] h-[80px] bg-pink-100 dark:bg-background flex justify-center items-center">{item.icon}</div>
+                <div className="w-[140px] h-[80px]  dark:bg-background flex justify-center items-center">{item.icon}</div>
                 </CardHeader>
                 <CardContent className="text-center"> 
                     <CardTitle className="mb-4">{item.title}</CardTitle>
