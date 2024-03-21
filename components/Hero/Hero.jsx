84545a459ca42d49f156f8ca1c474bad0bc4c911
chndrwali <chndrwali@gmail.com>
2024-03-21 17:29:48 +0700

    fix: display mobile

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -57,7 +57,7 @@ const Hero = () => {
     }, 2000);
   };
   return (
-    <section className="py-12 xl:py-24 h-[84vh] xl:pt-10 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none  ">
+    <section className="py-12 xl:py-24 h-[84vh] mb-14 xl:mb-10 xl:pt-10 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none  ">
       <div className="container mx-auto">
         <div className="flex justify-between gap-x-8 ">
           <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
